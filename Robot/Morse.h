3025f77e62d252e81390cd2e56b594a2e764ac84
Yahor <11egork@gmail.com>
2014-06-07 23:17:57 +0300

    Integrates with Visual Studio
    This code helps you to develop Arduino code within Visual Studio. Note,
    make arduino use 'external editor' in properties.

---
+++ Robot/Morse.h
@@ -0,0 +1,21 @@
+/*
+Morse.h - Library for flashing Morse code.
+Created by David A. Mellis, November 2, 2007.
+Released into the public domain.
+*/
+#ifndef MORSEH
+#define MORSEH
+
+#include "ArduinoFix.h"
+
+class Morse
+{
+public:
+    Morse(int pin);
+    void dot();
+    void dash();
+private:
+    int _pin;
+};
+
+#endif
